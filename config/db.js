9742e4e559a687a4bc21e8b4d602c94621c32706
Roberto Martins <roberto@actuar.group>
2020-09-29 16:19:14 -0300

    deletando arquivo db

--- config/db.js
+++
@@ -1,7 +0,0 @@
-var dbConfig = require('./default.json');
-
-module.exports = () => {
-    var db = {};
-
-    return mongoose.connect(dbConfig.bd.host, { useNewUrlParser: true, useUnifiedTopology: true });
-};
